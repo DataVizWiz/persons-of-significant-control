ccdb57978eeecbb23caffcbd545ffe16ecc62217
metin <metin@alisho.co.uk>
2026-01-23 21:51:25 +0000

    Lambda function enhancements

--- lambda_function.py
+++ lambda_function.py
@@ -2,19 +2,20 @@ import os
 import requests
 import boto3
 import json
-import zipfile
-import pandas as pd
 from datetime import datetime
 
 
-def download_from_api(file_name: str):
+def download_from_api(file_name: str) -> str:
     base_url = "https://download.companieshouse.gov.uk"
     url = f"{base_url}/{file_name}"
 
+    download_path = f"/tmp/{file_name}"
+
     with requests.get(url, stream=True) as r:
-        with open(file_name, "wb") as f:
+        with open(download_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
+    return download_path
 
 
 def upload_file_to_s3(client: object, file_path: str, bucket: str, zone: str):
@@ -30,23 +31,13 @@ def ingest_partition(partition: str):
     s3 = boto3.client("s3")
     today = datetime.today().strftime("%Y-%m-%d")
     zip_file = f"psc-snapshot-{today}_{partition}.zip"
-    download_from_api(zip_file)
-
-    with zipfile.ZipFile(zip_file, "r") as zf:
-        zf.extractall(".")
-
-    txt_file = zip_file.replace(".zip", ".txt")
-    init_df = pd.read_json(txt_file, lines=True)
-    normalized_df = pd.json_normalize(init_df["data"], sep="_")
-    csv_file = zip_file.replace(".zip", ".csv")
-    normalized_df.to_csv(csv_file, index=False)
-
-    for f in [zip_file, txt_file, csv_file]:
-        upload_file_to_s3(s3, f, "companies-house-psc", "raw")
+    f_path = download_from_api(zip_file)
+    upload_file_to_s3(s3, f_path, "companies-house-psc", "raw")
+    os.remove(f_path)
 
 
 def lambda_handler(event, context):
-    partition = "1of31"
+    partition = event.get("partition")
     ingest_partition(partition)
     msg = f"Partition ingested: {partition}"
     return {"statusCode": 200, "body": json.dumps(msg)}
--- scripts/test_lambda.py
+++ scripts/test_lambda.py
@@ -1,5 +1,5 @@
 import requests
 
-url = "https://2qv9hwa10j.execute-api.eu-west-2.amazonaws.com/prod?partition=1of31"
+url = "https://2qv9hwa10j.execute-api.eu-west-2.amazonaws.com/prod?partition=2of31"
 res = requests.get(url)
 print(res.text)
