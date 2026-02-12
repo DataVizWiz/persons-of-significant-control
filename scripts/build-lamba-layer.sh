3afb8074860ccd66bcb24b76005d8d28898704df
DataVizWiz <metin@alisho.co.uk>
2026-02-12 20:14:06 +0000

    Clear files

--- scripts/build-lamba-layer.sh
+++
@@ -1,7 +0,0 @@
-source .venv/bin/activate
-pip install -r requirements.txt
-
-mkdir -p python/lib
-cp -a .venv/lib/* python/lib
-zip -r python.zip python
-rm -rf python
--- scripts/test_lambda.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "https://2qv9hwa10j.execute-api.eu-west-2.amazonaws.com/prod?partition=2of31"
-res = requests.get(url)
-print(res.text)
